7e894e3099e021bba924d761bcb77a1acb55ff6b
josephh24 <josepsan2424@gmail.com>
2019-01-27 22:24:26 -0600

    Pagina de login con bootstrap 4

---
+++ js/validarBootstrap.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $('#reg_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+	       email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Porfavor escriba su correo electronico'
+                    },
+                    emailAddress: {
+                        message: 'Porfavor escriba un Email valido'
+                    }
+                }
+            },		
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'Porfavor escriba su password'
+                    },
+                    identical: {
+                        field: 'confirmPassword',
+                        message: 'Confirma tu password debajo - Escriba el mismo password porfavor'
+                    }
+                }
+            },
+            confirmPassword: {
+                validators: {
+                    notEmpty: {
+                        message: 'Porfavor escriba su password de nuevo'
+                    },
+                    identical: {
+                        field: 'password',
+                        message: 'El password debe de ser el mismo'
+                    }
+                }
+            },       
+        }
+    })
+		
+    .on('success.form.bv', function(e) {
+        $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+        $('#reg_form').data('bootstrapValidator').resetForm();
+        // Prevent form submission
+        e.preventDefault();
+
+        // Get the form instance
+        var $form = $(e.target);
+
+        // Get the BootstrapValidator instance
+        var bv = $form.data('bootstrapValidator');
+
+        // Use Ajax to submit form data
+        $.post($form.attr('action'), $form.serialize(), function(result) {
+            console.log(result);
+        }, 'json');
+    });
+});
